6ccc0d52748daa570e1aaa3e5bf639e31044a0ad
frabin2023 <frabin2023@gmail.com>
2025-02-15 18:12:55 -0800

    update test01

--- CISC 179 Python/W2/test01.py
+++ CISC 179 Python/W2/test01.py
@@ -2,3 +2,19 @@ import time
 for i in range(1,5):
     print(i, end="",flush=True)
 time.sleep(1)
+
+# print("----------------------------------------------------------------")
+# # Write your code here
+# print("127","0","0","1",sep=".")
+
+#type(1+"2")
+
+type(1.)
+
+type('A')
+
+type(500)
+
+type(True)
+
+type("False")
